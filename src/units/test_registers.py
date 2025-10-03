520f9d57dfc8b500aeb8df76e4a72a1f0daf702c
Kuba Sunderland-Ober <kuba@mareimbrium.org>
2025-10-04 00:38:48 +0200

    Significantly speed up the Registers class.

--- src/units/test_registers.py
+++ src/units/test_registers.py
@@ -1,29 +1,93 @@
-from src.z80 import registers
 import unittest
+
+from src.z80 import registers
+
+FLAGS = ("S", "Z", "F5", "H", "F3", "PV", "N", "C")
+PAIRS = ("AF", "BC", "DE", "HL")
+
 
 class TestZ80Registers(unittest.TestCase):
 
     def setUp(self):
         self.registers = registers.Registers(power=None)
-        
-    def test_double_register_access(self):
-        self.registers.H = 0x45
-        self.registers.L = 0x46
-        self.assertEqual(self.registers.HL, 0x4546)
-        
-    def test_condition_flags(self):
-        self.registers.F=0x0
-        self.registers.condition.C=True
-        self.assertEqual(self.registers.F, 0x01 << 0)
 
-        self.registers.F=0x0
-        self.registers.condition.S=True
-        self.assertEqual(self.registers.F, 0x01 << 7)  
-        
-        self.registers.F=0x0
-        self.registers.condition.PV=True
-        self.assertEqual(self.registers.F, 0x01 << 2)        
-        
-        
+    def test_register_power(self):
+        # not sure what it's for yet, but it should exist
+        self.assertTrue(hasattr(self.registers, "power"))
+
+    def test_register_pair_writes(self):
+        for pair in PAIRS:
+            h = pair[0]
+            l = pair[1]
+            for i in range(0x10000):
+                setattr(self.registers, pair, i)
+                self.assertEqual(getattr(self.registers, h), i >> 8)
+                self.assertEqual(getattr(self.registers, l), i & 0xFF)
+
+    def test_register_pair_reads(self):
+        for pair in PAIRS:
+            h = pair[0]
+            l = pair[1]
+            for i in range(0x10000):
+                setattr(self.registers, h, i >> 8)
+                setattr(self.registers, l, i & 0xFF)
+                self.assertEqual(getattr(self.registers, pair), i)
+
+    def test_condition_set(self):
+        for i, reg in enumerate(FLAGS):
+            self.registers.F = 0x0
+            setattr(self.registers.condition, reg, True)
+            self.assertEqual(self.registers.F, 0x01 << (7 - i))
+
+            for reg2 in FLAGS:
+                if reg2 != reg:
+                    self.assertEqual(bool(getattr(self.registers.condition, reg2)), False)
+
+    def test_condition_clear(self):
+        for i, reg in enumerate(FLAGS):
+            self.registers.F = 0xFF
+            setattr(self.registers.condition, reg, False)
+            self.assertEqual(self.registers.F, 0xFF - (0x01 << (7 - i)))
+
+            for reg2 in FLAGS:
+                if reg2 != reg:
+                    self.assertEqual(bool(getattr(self.registers.condition, reg2)), True)
+
+    def test_condition_set_notZ(self):
+        self.registers.F = 0
+        self.registers.condition.notZ = False
+        self.assertEqual(self.registers.F, 0x01 << (7 - 1))
+
+        for reg2 in FLAGS:
+            if reg2 != "Z":
+                self.assertEqual(bool(getattr(self.registers.condition, reg2)), False)
+
+    def test_condition_clear_notZ(self):
+        self.registers.F = 0xFF
+        self.registers.condition.notZ = True
+        self.assertEqual(self.registers.F, 0xFF - (0x01 << (7 - 1)))
+
+        for reg2 in FLAGS:
+            if reg2 != "Z":
+                self.assertEqual(bool(getattr(self.registers.condition, reg2)), True)
+
+    def check_condition_group(self, group: str):
+        condition = self.registers.condition
+
+        setattr(condition, group, True)
+        for reg in group:
+            self.assertTrue(getattr(condition, reg))
+
+        setattr(condition, group, False)
+        for reg in group:
+            self.assertFalse(getattr(condition, reg))
+
+    def test_condition_groups(self):
+        self.check_condition_group("SHN")
+        self.check_condition_group("HN")
+        self.check_condition_group("HNC")
+        self.check_condition_group("NC")
+
+
 if __name__ == '__main__':
     unittest.main()
--- src/z80/registers.py
+++ src/z80/registers.py
@@ -1,101 +1,315 @@
-class BitAccesser(object):
-    def __init__(self, bit_names, registers, reg):
-        object.__setattr__(self, "bits",
-                           dict(zip(bit_names, range(7, -1, -1))))
-        self.registers = registers
-        #self.registers = registers
-        self.reg = reg
-        
-    def __getattr__(self, b):
-        return (self.registers[self.reg] >>  self.bits[b]) &  1 
-    
-    def __setattr__(self, b, v):
-        if not b in self.bits:
-            object.__setattr__(self, b, v)
-            return
+from __future__ import annotations
 
-        if v:
-            self.registers[self.reg] = self.registers[self.reg] | (1 <<  self.bits[b])
+__all__ = ("Flags", "Registers")
+
+from typing import Any
+
+
+class Flags:
+    __slots__ = ("_regs")
+    _regs: Registers
+
+    # ["S", "Z", "F5", "H", "F3", "PV", "N", "C"]
+
+    def __init__(self, regs: Registers):
+        self._regs = regs
+
+    @property
+    def S(self):
+        return (self._regs.AF >> 7) & 1
+
+    @property
+    def Z(self):
+        return (self._regs.AF >> 6) & 1
+
+    @property
+    def notZ(self):
+        return ((self._regs.AF >> 6) & 1) ^ 1
+
+    @property
+    def F5(self):
+        return (self._regs.AF >> 5) & 1
+
+    @property
+    def H(self):
+        return (self._regs.AF >> 4) & 1
+
+    @property
+    def F3(self):
+        return (self._regs.AF >> 3) & 1
+
+    @property
+    def PV(self):
+        return (self._regs.AF >> 2) & 1
+
+    @property
+    def N(self):
+        return (self._regs.AF >> 1) & 1
+
+    @property
+    def C(self):
+        return self._regs.AF & 1
+
+    @S.setter
+    def S(self, val):
+        if val:
+            self._regs.AF |= 0x0080
         else:
-            self.registers[self.reg] = self.registers[self.reg] & ((1 <<  self.bits[b]) ^  0xFF)
-            
-class Registers(dict):
-    def __init__(self, *arg, **kw):
-        super(Registers, self).__init__(*arg, **kw)
+            self._regs.AF &= 0xFF7F
 
-        self.reset()
-        
+    @Z.setter
+    def Z(self, val):
+        if val:
+            self._regs.AF |= 0x0040
+        else:
+            self._regs.AF &= 0xFFBF
+
+    @notZ.setter
+    def notZ(self, val):
+        if val:
+            self._regs.AF &= 0xFFBF
+        else:
+            self._regs.AF |= 0x0040
+
+    @F5.setter
+    def F5(self, val):
+        if val:
+            self._regs.AF |= 0x0020
+        else:
+            self._regs.AF &= 0xFFDF
+
+    @H.setter
+    def H(self, val):
+        if val:
+            self._regs.AF |= 0x0010
+        else:
+            self._regs.AF &= 0xFFEF
+
+    @F3.setter
+    def F3(self, val):
+        if val:
+            self._regs.AF |= 0x0008
+        else:
+            self._regs.AF &= 0xFFF7
+
+    @PV.setter
+    def PV(self, val):
+        if val:
+            self._regs.AF |= 0x0004
+        else:
+            self._regs.AF &= 0xFFFB
+
+    @N.setter
+    def N(self, val):
+        if val:
+            self._regs.AF |= 0x0002
+        else:
+            self._regs.AF &= 0xFFFD
+
+    @C.setter
+    def C(self, val):
+        if val:
+            self._regs.AF |= 0x0001
+        else:
+            self._regs.AF &= 0xFFFE
+
+    @property
+    def SHN(self):
+        raise NotImplementedError()
+
+    @property
+    def HN(self):
+        raise NotImplementedError()
+
+    @property
+    def HNC(self):
+        raise NotImplementedError()
+
+    @property
+    def NC(self):
+        raise NotImplementedError()
+
+    @SHN.setter
+    def SHN(self, val):
+        # ["S", "Z", "F5", "H", "F3", "PV", "N", "C"]
+        if val:
+            self._regs.AF |= 0x0092
+        else:
+            self._regs.AF &= 0xFF6D
+
+    @HN.setter
+    def HN(self, val):
+        # ["S", "Z", "F5", "H", "F3", "PV", "N", "C"]
+        if val:
+            self._regs.AF |= 0x0012
+        else:
+            self._regs.AF &= 0xFFED
+
+    @HNC.setter
+    def HNC(self, val):
+        # ["S", "Z", "F5", "H", "F3", "PV", "N", "C"]
+        if val:
+            self._regs.AF |= 0x0013
+        else:
+            self._regs.AF &= 0xFFEC
+
+    @NC.setter
+    def NC(self, val):
+        # ["S", "Z", "F5", "H", "F3", "PV", "N", "C"]
+        if val:
+            self._regs.AF |= 0x0003
+        else:
+            self._regs.AF &= 0xFFFC
+
+
+class Registers:
+    __slots__ = ("PC", "SP", "IX", "IY", "I", "R",
+                 "AF", "AF_",
+                 "BC", "BC_",
+                 "DE", "DE_",
+                 "HL", "HL_",
+                 "condition", "flags",
+                 "HALT", "IFF", "IFF2", "IM",
+                 "power")
+
+    power: Any
+
+    def __init__(self, **kwargs):
+        self.PC = 0  # Program Counter (16bit)
+        self.SP = 0  # Stack Pointer (16bit)
+        self.IX = 0  # Index Register X (16bit)
+        self.IY = 0  # Index Register Y (16bit)
+        self.I = 0  # Interrupt Page Address (8bit)
+        self.R = 0  # Memory Refresh (8bit)
+
+        self.AF = 0  # Accumulator + Flags (16bit)
+        self.AF_ = 0  # Accumulator + Flags (16bit)
+
+        self.BC = 0  # General (16bit)
+        self.BC_ = 0  # General (16bit)
+
+        self.DE = 0  # General (16bit)
+        self.DE_ = 0  # General (16bit)
+
+        self.HL = 0  # General (16bit)
+        self.HL_ = 0  # General (16bit)
+
+        self.condition = Flags(self)
+        self.flags = self.condition
+
+        self.HALT = False  #
+        self.IFF = False  # Interrupt flip flop
+        self.IFF2 = False  # NM Interrupt flip flop
+        self.IM = False  # Interrupt mode
+
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
     def reset(self):
-        self["PC"] = 0 # Program Counter (16bit)
-        self["SP"] = 0 # Stack Pointer (16bit)
-        self["IX"] = 0 # Index Register X (16bit)
-        self["IY"] = 0 # Index Register Y (16bit)
-        self["I"] = 0  # Interrupt Page Address (8bit)
-        self["R"] = 0  # Memory Refresh (8bit)
+        self.__init__()
 
-        self["A"] = 0 # Accumulator (8bit)
-        self["F"] = 0 # Flags (8bit)
-        self["A_"] = 0 # Alt. Accumulator (8bit)
-        self["F_"] = 0 # Alt. Flags (8bit)
+    @property
+    def A(self) -> int:
+        return self.AF >> 8
 
-        self["B"] = 0 # General (8bit)
-        self["C"] = 0 # General (8bit)
-        self["B_"] = 0 # General (8bit)
-        self["C_"] = 0 # General (8bit)
+    @property
+    def F(self) -> int:
+        return self.AF & 0xFF
 
-        self["D"] = 0 # General (8bit)
-        self["E"] = 0 # General (8bit)
-        self["D_"] = 0 # General (8bit)
-        self["E_"] = 0 # General (8bit)
+    @property
+    def B(self) -> int:
+        return self.BC >> 8
 
-        self["H"] = 0 # General (8bit)
-        self["L"] = 0 # General (8bit)
-        self["H_"] = 0 # General (8bit)
-        self["L_"] = 0 # General (8bit)
+    @property
+    def C(self) -> int:
+        return self.BC & 0xFF
 
-        self["condition"] = BitAccesser(["S", "Z", "F5", "H", "F3", "PV", "N", "C"], self, "F")
-        
-        self['HALT']=False #
-        self['IFF']=False  # Interrupt flip flop
-        self['IFF2']=False  # NM Interrupt flip flop
-        self['IM']=False   # Iterrupt mode
+    @property
+    def D(self) -> int:
+        return self.DE >> 8
 
-    def __setattr__(self, attr, val):
-        if attr  in ["HL", "AF", "BC", "DE"]:
-            self[attr[0]] = val >> 8
-            self[attr[1]] = val &  0xFF
-        elif attr in ["IXH", "IYH"]:
-            self[attr[0:2]] = (self[attr[0:2]] & 0x00FF) | (val << 8)
-        elif attr in ["IXL", "IYL"]:
-            self[attr[0:2]] = (self[attr[0:2]] & 0xFF00) | val
-        else:
-            self[attr] = val
+    @property
+    def E(self) -> int:
+        return self.DE & 0xFF
 
-    def __getattr__(self, reg):
-        if reg in self:
-            return self[reg]
-        elif reg in ["HL", "AF", "BC", "DE"]:
-            return self[reg[0]] << 8 |  self[reg[1]]
-        elif reg in ["IXH", "IYH"]:
-            return self[reg[0:2]] >> 8
-        elif reg in ["IXL", "IYL"]:
-            return self[reg[0:2]] & 0xFF
-        else:
-            raise AttributeError("%s Not a known register."%reg)
-        
+    @property
+    def H(self) -> int:
+        return self.HL >> 8
+
+    @property
+    def L(self) -> int:
+        return self.HL & 0xFF
+
+    @property
+    def IXH(self) -> int:
+        return self.IX >> 8
+
+    @property
+    def IXL(self) -> int:
+        return self.IX & 0xFF
+
+    @property
+    def IYH(self) -> int:
+        return self.IY >> 8
+
+    @property
+    def IYL(self) -> int:
+        return self.IY & 0xFF
+
+    @A.setter
+    def A(self, val):
+        self.AF = (self.AF & 0x00FF) | ((val & 0xFF) << 8)
+
+    @F.setter
+    def F(self, val):
+        self.AF = (self.AF & 0xFF00) | (val & 0xFF)
+
+    @B.setter
+    def B(self, val):
+        self.BC = (self.BC & 0x00FF) | ((val & 0xFF) << 8)
+
+    @C.setter
+    def C(self, val):
+        self.BC = (self.BC & 0xFF00) | (val & 0xFF)
+
+    @D.setter
+    def D(self, val):
+        self.DE = (self.DE & 0x00FF) | ((val & 0xFF) << 8)
+
+    @E.setter
+    def E(self, val):
+        self.DE = (self.DE & 0xFF00) | (val & 0xFF)
+
+    @H.setter
+    def H(self, val):
+        self.HL = (self.HL & 0x00FF) | ((val & 0xFF) << 8)
+
+    @L.setter
+    def L(self, val):
+        self.HL = (self.HL & 0xFF00) | (val & 0xFF)
+
+    @IXH.setter
+    def IXH(self, val):
+        self.IX = (self.IX & 0x00FF) | ((val & 0xFF) << 8)
+
+    @IXL.setter
+    def IXL(self, val):
+        self.IX = (self.IX & 0xFF00) | (val & 0xFF)
+
+    @IYH.setter
+    def IYH(self, val):
+        self.IY = (self.IY & 0x00FF) | ((val & 0xFF) << 8)
+
+    @IYL.setter
+    def IYL(self, val):
+        self.IY = (self.IY & 0xFF00) | (val & 0xFF)
+
     def __getitem__(self, reg):
-        if reg in ["BC", "HL", "DE", "AF", "IXH", "IXL", "IYH", "IYL"]:
-            return getattr(self, reg)
-        else:
-            return super(Registers, self).__getitem__(reg)
-        
+        return getattr(self, reg)
+
     def __setitem__(self, reg, val):
-        if reg in ["BC", "HL", "DE", "AF", "IXH", "IXL", "IYH", "IYL"]:
-            return setattr(self, reg, val)
-        else:
-            return super(Registers, self).__setitem__(reg, val)
-    
+        return setattr(self, reg, val)
+
     @classmethod
     def create(cls):
         return cls()
-        
